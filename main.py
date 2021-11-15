811ced64dfdb13550180f3983e523f4fda808f34
kevinozkaraca <kevin.ozkaraca@gmail.com>
2021-11-15 15:37:11 +0300

    Fond, icones et ajustement + readme

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import tkinter
-# Taille de la fenetre
+# Taille des éléments
 width_fenetre = "800"
 height_fenetre = "600"
-taille_images = "200"
-# Images du Shi-Fu-Mi
-images_ciseaux = tkinter.PhotoImage(file = r"imagesShifumi/ciseauxCouleur.PNG")
-images_feuille = tkinter.PhotoImage(file = r"imagesShifumi/feuilleCouleur.PNG")
-images_pierre = tkinter.PhotoImage(file = r"imagesShifumi/pierreCouleur.PNG")
-images_de_fond = tkinter.PhotoImage(file = r"imagesShifumi/Présentation.PNG")
+taille_images = "150"
 # Affichage de la fenetre
 fenetre = tkinter.Tk()
 fenetre.title("Shi-Fu-Mi")
 fenetre.geometry(f"{width_fenetre}x{height_fenetre}")
 fenetre.resizable(width=False, height=False)
-fenetre.configure(background="#A1D2E1") 
-fenetre.iconphoto(False, tkinter.PhotoImage(file='imagesShifumi/icone.png'))
 
+fenetre.configure(background="#A1D2E1")
+fenetre.iconphoto(False, tkinter.PhotoImage(file=r"imagesShifumi/icone.png"))
+# Images du Shi-Fu-Mi
+image_ciseaux = tkinter.PhotoImage(file=r"imagesShifumi/ciseauxCouleur.png")
+image_feuille = tkinter.PhotoImage(file=r"imagesShifumi/feuilleCouleur.png")
+image_pierre = tkinter.PhotoImage(file=r"imagesShifumi/pierreCouleur.png")
+image_de_fond = tkinter.PhotoImage(file=r"imagesShifumi/Présentation.png")
 # Boutons du Shi-Fu-Mi
-boutonCiseaux = tkinter.Button(fenetre,image=images_ciseaux, bd=5, width=taille_images,height=taille_images)
-boutonFeuille = tkinter.Button(fenetre,image=images_feuille, bd=5, width=taille_images,height=taille_images)
-boutonPierre = tkinter.Button(fenetre,image=images_pierre, bd=5, width=taille_images,height=taille_images)
-boutonCiseaux.place(x=50,y=280)
-boutonPierre.place(x=300,y=280)
-boutonFeuille.place(x=550,y=280)
+fond_de_shifumi = tkinter.Label( fenetre, image = image_de_fond) 
+fond_de_shifumi.place(x = 0, y = 0)
+boutonCiseaux = tkinter.Button(fenetre, image=image_ciseaux, bd=5, width=taille_images, height=taille_images)
+boutonFeuille = tkinter.Button(fenetre, image=image_feuille, bd=5, width=taille_images, height=taille_images)
+boutonPierre = tkinter.Button(fenetre, image=image_pierre, bd=5, width=taille_images, height=taille_images)
+boutonCiseaux.place(x=88, y=240)
+boutonPierre.place(x=324, y=240)
+boutonFeuille.place(x=561, y=240)
+
 # Mainloop
 fenetre.mainloop()
